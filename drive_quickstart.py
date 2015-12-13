2821bcf9dea314420823f6442ac27095c02e5d98
Andy Pernsteiner <andypern@gmail.com>
2015-12-12 20:06:41 -0500

    changed handling for >2 versions

--- drive_quickstart.py
+++ drive_quickstart.py
@@ -85,29 +85,22 @@ def cleanup():
         print('No files found.')
     else:
         returnedResults = len(items)
+        print("results: %s" %(returnedResults))
         for item in items:
             try:
                 if "jhong" in item['lastModifyingUser']['emailAddress']:
                     
                     revs = service.revisions().list(fileId=item['id']).execute()
-                    #only do things to files that have 2 rev's..those w/ more may require tender care..
-                    if len(revs['items']) == 2:
-                        #that means we have a backup, usually the [0] index is the older one, but lets make sure
-                        if 'vvv' in revs['items'][1]['originalFilename']:
-                            try:
-                                revId = revs['items'][1]['id']
-                                oldFileName = revs['items'][0]['originalFilename']
-                                oldFileModDate = revs['items'][0]['modifiedDate']
-                                print("deleting revision " + revs['items'][1]['originalFilename'])
-                                service.revisions().delete(
-                                    fileId=item['id'], revisionId=revId).execute()
-                            except errors.HttpError, error:
-                                print('http error %s' %(error))
-
-                            #now, rename the 'vvv' file to original name, which we can get from the other rev
+                    print ("working on : %s" %(item['title']))
+                    #Items with only 1 revision may have been 'cleaned' but not renamed, do something with them
+                    if len(revs['items']) == 1:
+                        if not 'vvv' in revs['items'][0]['originalFilename']:
+                            print("replacing with original filename: %s" %(revs['items'][0]['originalFilename']))
                             try:
                                 #first, pull all file metadata
                                 objFile = service.files().get(fileId=item['id']).execute()
+                                oldFileName = revs['items'][0]['originalFilename']
+                                oldFileModDate = revs['items'][0]['modifiedDate']
                                 #set the attributes we want to change
                                 objFile['title'] = oldFileName
                                 objFile['modifiedDate'] = oldFileModDate
@@ -121,12 +114,94 @@ def cleanup():
                                 
                             except errors.HttpError, error:
                                 print('An error occurred: %s' % error)
+                    # #items with 2 revisions mean that we can just delete the current one, and rename with the previous one
+                    # if len(revs['items']) == 2:
+                    #     #that means we have a backup, usually the [0] index is the older one, but lets make sure
+                    #     if 'vvv' in revs['items'][1]['originalFilename']:
+                    #         try:
+                    #             revId = revs['items'][1]['id']
+                    #             oldFileName = revs['items'][0]['originalFilename']
+                    #             oldFileModDate = revs['items'][0]['modifiedDate']
+                    #             print("deleting revision " + revs['items'][1]['originalFilename'])
+                    #             service.revisions().delete(
+                    #                 fileId=item['id'], revisionId=revId).execute()
+                    #         except errors.HttpError, error:
+                    #             print('http error %s' %(error))
+
+                    #         #now, rename the 'vvv' file to original name, which we can get from the other rev
+                    #         try:
+                    #             #first, pull all file metadata
+                    #             objFile = service.files().get(fileId=item['id']).execute()
+                    #             #set the attributes we want to change
+                    #             objFile['title'] = oldFileName
+                    #             objFile['modifiedDate'] = oldFileModDate
+                    #             print("renaming to :%s" %(oldFileName))
+
+                    #             #execute the change, note we need to set the 'setModifiedDate' flag
+                    #             updated_file = service.files().update(
+                    #                 fileId=item['id'],
+                    #                 body=objFile,
+                    #                 modifiedDateBehavior='fromBody',
+                    #                 setModifiedDate='true').execute()
+                                
+                    #         except errors.HttpError, error:
+                    #             print('An error occurred: %s' % error)
+                 #items with 2 or more revisions require that we count the revision, delete most current, and replace with previous. Except
+                 # when more than one revision is 'bad', in which case we have to walk backwards until we find a good one.  We don't want to go all the way back in case there are
+                 # multiple 'good' versions.
+                 # also, we have to find the 'originalFilename' differently, because it may still be the 'bad' one, so we'll have to use from rev's.
+                if len(revs['items']) >= 2:
+                    #set a 'fixed' flag so we can break out of the loop after we've fixed
+                    fixedFlag = 0
+                    #note: we can't walk through it in array order, we have to walk in backwards order
+                    arrayIndex = len(revs['items']) - 1
+                   
+                    sys.exit
+                    while (arrayIndex >=0):
+                        #go through each rev, look for bad ones and delete
+                        if 'vvv' in revs['items'][arrayIndex]['originalFilename']:
+                            try:
+                                revId = revs['items'][arrayIndex]['id']
+                                print("deleting revision " + revs['items'][arrayIndex]['originalFilename'])
+                                service.revisions().delete(
+                                    fileId=item['id'], revisionId=revId).execute()
+                                arrayIndex -= 1
+                            except errors.HttpError, error:
+                                print('http error %s' %(error))
+                        else:
+                            if fixedFlag == 0:
+                            #means we didn't find vvv, so we can use this revision
+                            #now, rename the 'vvv' file to original name, which we can get from the current rev
+                                try:
+                                    #first, pull all file metadata
+                                    objFile = service.files().get(fileId=item['id']).execute()
+                                    #set the attributes we want to change
+                                    objFile['title'] = revs['items'][arrayIndex]['originalFilename']
+                                    objFile['modifiedDate'] = revs['items'][arrayIndex]['modifiedDate']
+                                    print("renaming to %s" %(objFile['title']))
+                                    #execute the change, note we need to set the 'setModifiedDate' flag
+                                    updated_file = service.files().update(
+                                        fileId=item['id'],
+                                        body=objFile,
+                                        modifiedDateBehavior='fromBody',
+                                        setModifiedDate='true').execute()
+                                    #since we were able to rename, set fixedFlag so this doesn't happen again
+                                    fixedFlag = 1
+                                    arrayIndex -= 1
+                                    print("renaming done")
+                                    break
+                                    
+                                except errors.HttpError, error:
+                                    print('An error occurred: %s' % error)
+                                    pass
+
 
             except KeyError:
                 print ("key error..skip")
+                print("file ID was; %s" %(item['id']))
             except UnicodeEncodeError:
                 print ("unicode error, skip")
             #print (item['title'])
 
-if returnedResults == 1000:
+if returnedResults > 100:
     cleanup()
