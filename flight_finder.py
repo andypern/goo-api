289ec07abc5b173132f99bb3f9578106445db46f
Andy Pernsteiner <andypern@gmail.com>
2016-06-02 13:55:52 -0400

    updates

--- flight_finder.py
+++ flight_finder.py
@@ -4,7 +4,7 @@ from googleapiclient.discovery import build
 
 
 QPX_API_VERSION = 'v1'
-DEVELOPER_KEY_FILE =  "/Users/apernsteiner/Desktop/GCE/flight.key"
+DEVELOPER_KEY_FILE =  "/Users/andypern/Desktop/GCE/flight.key"
 
 fh = open(DEVELOPER_KEY_FILE, 'r')
 
@@ -20,10 +20,16 @@ def main():
       "request": {
         "slice": [
           {
-            "origin": "JFK",
-            "destination": "CDG",
-            "date": "2016-02-24"
-          }
+            "origin": "BOS",
+            "destination": "PVG",
+            "date": "2016-09-27"
+          },
+          {
+            "origin": "PVG",
+            "destination": "BOS",
+            "date": "2016-10-07"
+          },
+
         ],
         "passengers": {
           "adultCount": 1,
@@ -32,21 +38,48 @@ def main():
           "childCount": 0,
           "seniorCount": 0
         },
-        "solutions": 20
+        "solutions": 100
       }
     }
   )
 
   response = request.execute()
-  pprint.pprint(response)
+  #pprint.pprint(response)
 
-  
+  for trip in response['trips']['tripOption']:
+    pprint.pprint(trip)
+    #########
+    #pricing
+    #
+    totalPrice =  trip['saleTotal']
+    print totalPrice
+    #
+    #now dig into the details on the pricing
+    # you get trip['pricing'] as an array, although it only has 1 element (dict)
+    #
+    #for prices in trip['pricing']:
+
+
+
+
+    #each segment becomes a slice i believe..
+
+    #for tripSlice in trip['slice']:
+    #  print tripSlice['']
+
+
+
+    # airline = tripSlice['carrier']
+    # fare = trip['saleFareTotal']
+    # tax = trip['saleTaxTotal']
+    # print "found this: %s , %s , %s, %s" %(totalPrice, airline, fare, tax)
+
 if __name__ == '__main__':
   main()
 
 
-  # resource = client.
-  # request = resource.list(source='public', country='US')
-  # response = request.execute()
-  # pprint.pprint(response)
+    # resource = client.
+    # request = resource.list(source='public', country='US')
+    # response = request.execute()
+    # pprint.pprint(response)
 
